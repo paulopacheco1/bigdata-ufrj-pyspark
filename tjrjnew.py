94d5c77c05698a72c0084278f41df9b804b9c2ca
WeValueBr <86888209+WeValueBr@users.noreply.github.com>
2021-12-05 17:23:54 -0300

    NOVA ATUALIZAÇÃO

--- tjrjnew.py
+++ tjrjnew.py
@@ -6,7 +6,7 @@ import pymongo
 tribunal = 'Tribunal de Justiça do Rio de Janeiro'
 uf = 'RJ'
 
-pdf_file = open('./20211110CAPDJETJRJ.pdf', 'rb')
+pdf_file = open('./20211111CAPDJETJRJ.pdf', 'rb')
 
 read_pdf = PyPDF2.PdfFileReader(pdf_file)
 
@@ -34,13 +34,13 @@ def isNumProcesso(cpf):
 
     return bool(re.match(pattern, cpf))
 
-#myclient = pymongo.MongoClient("mongodb+srv://admin:admin1324@bigdataufrj.lpm6m.mongodb.net/projetoBigData?retryWrites=true&w=majority")
-#mydb = myclient["projetoBigData"]
+myclient = pymongo.MongoClient("mongodb+srv://admin:admin1324@bigdataufrj.lpm6m.mongodb.net/projetoBigData?retryWrites=true&w=majority")
+mydb = myclient["projetoBigData"]
 
-#mycol = mydb["processos"]
-
-for x in range(1):
-    page = read_pdf.getPage(13)
+mycol = mydb["processos"]
+print(number_of_pages)
+for x in range(number_of_pages):
+    page = read_pdf.getPage(x)
 
     #extrai apenas o texto
     page_content = page.extractText()
@@ -59,7 +59,6 @@ for x in range(1):
         if (isNumProcesso(numberProcesso) == True):   
             print("============PROCESSO ("+numberProcesso+")===========")
             partes = processo[26:].split(' X ')
-
            
             andamento = processo[26:]
             numberProcessoOld  = ''
@@ -76,37 +75,53 @@ for x in range(1):
                 nome = ''
 
                 aAdvogados = []
-
-               
-
+                
                 if(parte.find('(') != -1):
                     if(parte[0] == '('):
-                       numberProcessoOld = parte[1:17]      
-                       nome = parte[18:parte[18:].index('(')+18].strip()        
-                       advogados = parte[parte[18:].index('(')+18:]                  
+                        numberProcessoOld = parte[1:17] 
+                        if(parte[18:].find('(') != -1):     
+                            nome = parte[18:parte[18:].index('(')+18].strip()        
+                            sAdvogados = parte[parte[18:].index('(')+18:]   
+                                    
                     else:
                         nome = parte[0:parte.index('(')].strip()
-                        advogados = parte[parte.index('('):]
-
-                    advogados = advogados.replace('(Adv(s).','').replace('Dr(a).','').strip()
+                        sAdvogados = parte[parte.index('('):]
                         
-                    advogados = advogados.split(',')
+
+                    
+                    if(sAdvogados.find('(Adv(s).') != -1):
+                        sAdvogados = sAdvogados.replace('(Adv(s).','').strip()
+
+                    if(sAdvogados.find('Dr(a).') != -1):
+                        sAdvogados = sAdvogados.replace('Dr(a).','').strip()
+                        
+                    advogados = sAdvogados.split(',')
                     
                     for advogado in advogados:
                         if(advogado.find('OAB/') != -1):
-                            advogado = advogado[0:advogado.index(')')+1].strip()
-                            
-                            advogado = advogado.split('(')
-                            nomeAdvogado= advogado[0].replace(':','').strip()
-                            oab = advogado[1].replace(')','').replace('OAB/','').strip()
-                            andamento = andamento.replace(oab,'').replace(nomeAdvogado,'').strip()
+                            if(advogado.find(')') != -1):
+                                advogado = advogado[0:advogado.index(')')+1].strip()
+                                
+                                advogado = advogado.split('(')
+                                nomeAdvogado= advogado[0].replace(':','').strip()
 
-                            if(len(nomeAdvogado) > 0): 
-    
-                                aAdvogados.append({
-                                    'nome':nomeAdvogado,
-                                    'oab':oab
-                                })     
+                                oab = ''
+                                
+                                if(len(advogado) > 1):
+                                    if(advogado[1].find(')') != -1):
+                                        oab = advogado[1].replace(')','').replace('OAB/','').strip()
+                                    
+                                    if(oab.find('OAB/') != -1):
+                                        oab = oab.replace('OAB/','').strip()
+                                
+                                andamento = andamento.replace(oab,'').replace(nomeAdvogado,'').strip()
+
+                                if(len(nomeAdvogado) > 0): 
+        
+                                    aAdvogados.append({
+                                        'nome':nomeAdvogado,
+                                        'oab':oab
+                                    })     
                 else:
                     if(parte[0:].find(':') != -1):
                         nome = parte[0:parte.index(':')]
@@ -124,14 +139,25 @@ for x in range(1):
                         }
                     )    
             
-            print({
-                'numJustica':numberProcesso,
-                'partes':aPartes,
-                'numJusticaAntigo':numberProcessoOld,                
-                'tribunal':tribunal,
-                'uf':uf,
-                'andamentos':[{andamento}]
-            })
+            mydoc = mycol.count_documents({ "numJustica": numberProcesso})
+            
+            if(mydoc == 0):
+                x = mycol.insert_one(
+                    {
+                        'numJustica':numberProcesso,
+                        'partes':aPartes,
+                        'numJusticaAntigo':numberProcessoOld,
+                        'andamentos':[{'descricao':andamento}],
+                        'tribunal':tribunal,
+                        'uf':uf
+                    }
+                )
+                print("cadastrado")
+            else:
+                print("ja esta cadastrado")
+               
+            
+            
             
         
 
